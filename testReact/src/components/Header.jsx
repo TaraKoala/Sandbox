1264e33f3f49ecd2dd784453331e757593e0fad6
TaraKoala <75086178+TaraKoala@users.noreply.github.com>
2023-07-20 13:10:02 +0200

    updated react files

---
+++ testReact/src/components/Header.jsx
@@ -0,0 +1,14 @@
+//import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="./assets/troll-face.png"
+                className="header--image"
+            />
+            <h2 >Meme Generator</h2>
+            <h4>React Course - Project</h4>
+        </header>
+    )
+}
