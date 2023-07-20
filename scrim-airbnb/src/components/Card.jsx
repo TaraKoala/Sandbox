ba6ebfcf25cf68175c2a55c938c6be119575aae4
TaraKoala <75086178+TaraKoala@users.noreply.github.com>
2023-07-20 13:09:33 +0200

    updated react files

---
+++ scrim-airbnb/src/components/Card.jsx
@@ -0,0 +1,32 @@
+//import React from "react"
+import starImage from "../assets/star.png"
+
+export default function Card(props) {
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+    
+    return (
+        <div className="card">
+            {
+                badgeText && 
+                <div className="card--badge">{badgeText}</div>
+            }
+            <img 
+                src={props.item.coverImg}  className="card--image" />
+            <div className="card--stats">
+                <img src={starImage} className="card--star" />
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
+            </div>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price">
+                <span className="bold">From ${props.price}</span> / person
+            </p>
+        </div>
+    )
+}
